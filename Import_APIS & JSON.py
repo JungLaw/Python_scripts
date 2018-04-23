318fc0e2eef9ceae18f79de4939649454e53a01e
JungLaw <larryjung@gmail.com>
2018-04-23 17:09:57 -0400

    Update Import_APIS & JSON.py

--- Import_APIS & JSON.py
+++ Import_APIS & JSON.py
@@ -1,1 +1,37 @@
+'''
+(1) 
+
+TASK: 
+- json stored in working directory as 'snakes.json'. 
+- load to python environment: 
+
+'''
+
+import json
+
+# context manager: open a connection to the file and use the function json.load to load the JSON.
+with open('snakes.json', r) as json_file:    json_data = json.load(json_file)
+
+# file type
+type(json_data)   
+
+# print items
+for key, value in json_data.items():
+    print(key + ':", value)
+
+                                               
+'''
+(2) 
+
+
+"""
+
+
+# Load JSON: json_data
+with open("a_movie.json") as json_file:
+    json_data = json.load(json_file)
+
+# Print each key-value pair in json_data
+for k in json_data.keys():
+    print(k + ': ', json_data[k])
 

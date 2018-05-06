893c2d6eb42af9c2606cf5a28b5767148a56f90d
JungLaw <larryjung@gmail.com>
2018-05-06 09:27:10 -0400

    Update ManipDF_Stacking & Unstacking.py

--- ManipDF_Stacking & Unstacking.py
+++ ManipDF_Stacking & Unstacking.py
@@ -1,1 +1,11 @@
+=======
+(2) SETTING & SORTING A MULTI-INDEX
 
+'''
+INSTRUCTIONS
+- Create a MultiIndex by setting the index to be the columns ['state', 'month'].
+- Sort the MultiIndex using the .sort_index() method.
+- Print the sales DataFrame. 
+'''
+
+# Reference/Checks:
